0ced8773a1b45be474d1fe817b9dfe9e797c7424
Aurélien Rouëné <aurelien@devops-now.com>
2018-06-06 15:12:15 +0200

    New file

---
+++ vslogger.go
@@ -0,0 +1,65 @@
+package vslogger
+
+import (
+        "log"
+        "fmt"
+        "io"
+)
+
+const (
+        Ldate         = log.Ldate
+        Ltime         = log.Ltime
+        Lmicroseconds = log.Lmicroseconds
+        Llongfile     = log.Llongfile
+        Lshortfile    = log.Lshortfile
+        LUTC          = log.LUTC
+        LstdFlags     = log.LstdFlags
+)
+
+type Logger struct {
+        log *log.Logger
+        level LogLevel
+}
+
+type LogLevel uint
+
+const (
+        LogDebug   LogLevel = 1
+        LogError   LogLevel = 2
+        LogWarning LogLevel = 3
+        LogInfo    LogLevel = 4
+)
+
+func New(level LogLevel, out io.Writer, prefix string, flag int) *Logger {
+        return &Logger{
+                log: log.New(out, prefix, flag),
+                level: level }
+}
+
+func (l *Logger) Debug(v ...interface{}) {
+        if l.level >= LogDebug {
+                v = append([]interface{}{"[D]"}, v...)
+                l.log.Output(2, fmt.Sprintln(v...))
+        }
+}
+
+func (l *Logger) Error(v ...interface{}) {
+        if l.level >= LogError {
+                v = append([]interface{}{"[E]"}, v...)
+                l.log.Output(2, fmt.Sprintln(v...))
+        }
+}
+
+func (l *Logger) Warning(v ...interface{}) {
+        if l.level >= LogWarning {
+                v = append([]interface{}{"[W]"}, v...)
+                l.log.Output(2, fmt.Sprintln(v...))
+        }
+}
+
+func (l *Logger) Info(v ...interface{}) {
+        if l.level >= LogInfo {
+                v = append([]interface{}{"[I]"}, v...)
+                l.log.Output(2, fmt.Sprintln(v...))
+        }
+}
